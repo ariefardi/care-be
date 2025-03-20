4717fe97e7e2ef2935bfe6f249450d0a66762f35
ariefardi <arief.ardi@bisnis.com>
2025-03-20 10:14:13 +0700

    push docker changing migration

--- seeds/01_seed_roles.ts
+++ seeds/01_seed_roles.ts
@@ -6,14 +6,14 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("roles").insert([
-        { id: 1, role_name: "Applied", createdAt: new Date(), updatedAt: new Date() },
-        { id: 2, role_name: "Contacted", createdAt: new Date(), updatedAt: new Date() },
-        { id: 3, role_name: "Interview Scheduled", createdAt: new Date(), updatedAt: new Date() },
-        { id: 4, role_name: "Candidate Rejected", createdAt: new Date(), updatedAt: new Date() },
-        { id: 5, role_name: "Offer Made", createdAt: new Date(), updatedAt: new Date() },
-        { id: 6, role_name: "Offer Accepted", createdAt: new Date(), updatedAt: new Date() },
-        { id: 7, role_name: "Offer Rejected", createdAt: new Date(), updatedAt: new Date() },
-        { id: 8, role_name: "Interview Done", createdAt: new Date(), updatedAt: new Date() },
-        { id: 9, role_name: "Hired", createdAt: new Date(), updatedAt: new Date() },
+        { id: 1, role_name: "Software Engineer", createdAt: new Date(), updatedAt: new Date() },
+        { id: 2, role_name: "Fullstack Developer", createdAt: new Date(), updatedAt: new Date() },
+        { id: 3, role_name: "QA Engineer", createdAt: new Date(), updatedAt: new Date() },
+        { id: 4, role_name: "Data Scientist", createdAt: new Date(), updatedAt: new Date() },
+        { id: 5, role_name: "Data Engineer", createdAt: new Date(), updatedAt: new Date() },
+        { id: 6, role_name: "Project Manager", createdAt: new Date(), updatedAt: new Date() },
+        { id: 7, role_name: "Database Engineer", createdAt: new Date(), updatedAt: new Date() },
+        { id: 8, role_name: "Talent Acquisition", createdAt: new Date(), updatedAt: new Date() },
+        { id: 9, role_name: "Backend Developer", createdAt: new Date(), updatedAt: new Date() },
     ]);
 }
